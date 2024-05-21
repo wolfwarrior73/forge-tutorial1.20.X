a0170521123bac3515c9e1b9fa363c2adb482a81
wolfwarrior73 <yosefdovidwolf@gmail.com>
2024-05-21 19:27:01 -0400

    Added first item!

--- src/main/java/net/yosef/tutorialmod/TutorialMod.java
+++ src/main/java/net/yosef/tutorialmod/TutorialMod.java
@@ -40,6 +40,7 @@ public class TutorialMod {
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
             event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
         }
 
     }
