1a82704eb947019c0d09eae66eab86141639f157
wolfwarrior73 <yosefdovidwolf@gmail.com>
2024-05-22 19:30:12 -0400

    Added first 2 blocks and seperate creative tab

--- src/main/java/net/yosef/tutorialmod/TutorialMod.java
+++ src/main/java/net/yosef/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.yosef.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.yosef.tutorialmod.block.ModBlocks;
+import net.yosef.tutorialmod.item.ModCreativeModTabs;
+import net.yosef.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -12,7 +15,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.yosef.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -24,7 +26,10 @@ public class TutorialMod {
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -36,13 +41,11 @@ public class TutorialMod {
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.SAPPHIRE);
             event.accept(ModItems.RAW_SAPPHIRE);
         }
-
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
