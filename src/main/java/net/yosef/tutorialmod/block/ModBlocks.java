16ebf2937e7d6dccc44a8779d34c4adff4d96321
wolfwarrior73 <yosefdovidwolf@gmail.com>
2024-05-27 11:36:34 -0400

    added a custom block (sound block)

--- src/main/java/net/yosef/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/yosef/tutorialmod/block/ModBlocks.java
@@ -13,6 +13,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.yosef.tutorialmod.TutorialMod;
+import net.yosef.tutorialmod.block.custom.SoundBlock;
 import net.yosef.tutorialmod.item.ModItems;
 
 import java.util.function.Supplier;
@@ -38,6 +39,9 @@ public class ModBlocks {
     public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)
                     .strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
+    public static final RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block",
+            () -> new SoundBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
--- src/main/java/net/yosef/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/yosef/tutorialmod/item/ModCreativeModTabs.java
@@ -31,6 +31,8 @@ public class ModCreativeModTabs {
                         pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
 
+                        pOutput.accept(ModBlocks.SOUND_BLOCK.get());
+
                     })
                     .build());
 
